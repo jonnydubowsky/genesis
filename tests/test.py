38086ee1f5dd95e4bede4c26727b512fe6e99b14
Lefteris Karapetsas <lefteris@refu.co>
2016-03-02 18:29:29 +0100

    deploy.js is now one click deployment

--- tests/test.py
+++ tests/test.py
@@ -11,10 +11,13 @@ import subprocess
 from datetime import datetime
 datetime.utcnow()
 
+
 def compile_contracts(solc):
     print("Compiling the DAO contract...")
 
-    contracts_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    contracts_dir = os.path.dirname(
+        os.path.dirname(os.path.realpath(__file__))
+    )
     dao_contract = os.path.join(contracts_dir, "DAO.sol")
     if not os.path.isfile(dao_contract):
         print("DAO contract not found at {}".format(dao_contract))
@@ -25,34 +28,50 @@ def compile_contracts(solc):
     data = subprocess.check_output([
         solc,
         os.path.join(contracts_dir, "DAO.sol"),
+        "--optimize",
         "--combined-json",
         "abi,bin"
     ])
     res = json.loads(data)
     contract = res["contracts"]["DAO"]
     DAOCreator = res["contracts"]["DAO_Creator"]
-    return contract["abi"], contract["bin"], DAOCreator["abi"], DAOCreator["bin"]
+    return (
+        contract["abi"], contract["bin"], DAOCreator["abi"], DAOCreator["bin"]
+    )
+
 
 def create_deploy_js(dabi, dbin, cabi, cbin):
     print("Rewritting deploy.js using the compiled contract...")
     f = open("deploy.js", "w")
     f.write(
-        """// geth --networkid 123 --nodiscover --maxpeers 0 --genesis ./genesis_block.json --datadir ./data console 2>> out.log.geth
+        """// geth --networkid 123 --nodiscover --maxpeers 0 --genesis ./genesis_block.json --datadir ./data  js deploy.js 2>>out.log.geth
 
-console.log("unlocking accounts")
-personal.unlockAccount(web3.eth.accounts[0], "Write here a good, randomly generated, passphrase!");
-personal.unlockAccount(web3.eth.accounts[1], "Write here a good, randomly generated, passphrase!");
+console.log("unlocking account");
+personal.unlockAccount(
+    web3.eth.accounts[0],
+    "Write here a good, randomly generated, passphrase!"
+);
 
 function checkWork() {
-    if (eth.getBlock("pending").transactions.length > 0) {
-        if (eth.mining) return;
-        console.log("== Pending transactions! Mining...");
+    if (!eth.mining) {
+       setTimeout(function() {
+            miner.stop(0);
+        }, 3000);
         miner.start(1);
     } else {
-        miner.stop(0);  // This param means nothing
-        console.log("== No transactions! Mining stopped.");
+        miner.stop(0);
     }
+   // if (eth.getBlock("pending").transactions.length > 0) {
+   //     if (eth.mining) return;
+   //     console.log("== Pending transactions! Mining...");
+   //     miner.start(1);
+   // } else {
+   //     miner.stop(0);  // This param means nothing
+   //     console.log("== No transactions! Mining stopped.");
+   // }
 }
+
+var _defaultServiceProvider = web3.eth.accounts[0];
 
 var daoContract = web3.eth.contract(""")
     f.write(dabi)
@@ -62,18 +81,17 @@ console.log("Creating DAOCreator Contract");
 var creatorContract = web3.eth.contract(""")
     f.write(cabi)
     f.write(""");
-    var _daoCreatorContract = creatorContract.new({from: web3.eth.accounts[0], data: '""");
+var _daoCreatorContract = creatorContract.new({from: web3.eth.accounts[0], data: '""");
     f.write(cbin)
     f.write("""',
 gas: 3000000
    }, function(e, contract){
        if (e) {
-	console.log(e+" at DAOCreator creation!");
+           console.log(e+" at DAOCreator creation!");
        }
        if (typeof contract.address != 'undefined') {
            console.log('DAOCreator mined! address: ' + contract.address + ' transactionHash: ' + contract.transactionHash);
            checkWork();
-           var _defaultServiceProvider = web3.eth.acounts[0]/* var of type address here */ ;
            var dao = daoContract.new(
                _defaultServiceProvider,
                contract.address,
@@ -88,7 +106,8 @@ gas: 3000000
             gas: 3000000,
             gasPrice: 500000000000
    }, function(e, contract){
-    console.log(e, contract);
+    // funny thing, without this geth hangs
+    console.log("At DAO creation callback");
     if (typeof contract.address != 'undefined') {
          console.log('DAO Contract mined! address: ' + contract.address + ' transactionHash: ' + contract.transactionHash);
     }

b2c3993cb0d530906be679cb34acc78646654333
Stephan Tual <stephan.tual@ursium.com>
2016-03-02 14:52:26 +0000

    renamed _value to _amount

--- Token.sol
+++ Token.sol
@@ -38,32 +38,32 @@ contract TokenInterface {
     /// @return The balance
     function balanceOf(address _owner) constant returns (uint256 balance);
 
-    /// @notice send `_value` tokens to `_to` from `msg.sender`
+    /// @notice send `_amount` tokens to `_to` from `msg.sender`
     /// @param _to The address of the recipient
-    /// @param _value The amount of tokens to be transfered
+    /// @param _amount The amount of tokens to be transfered
     /// @return Whether the transfer was successful or not
-    function transfer(address _to, uint256 _value) returns (bool success);
+    function transfer(address _to, uint256 _amount) returns (bool success);
 
-    /// @notice send `_value` tokens to `_to` from `_from` on the condition it is approved by `_from`
+    /// @notice send `_amount` tokens to `_to` from `_from` on the condition it is approved by `_from`
     /// @param _from The address of the sender
     /// @param _to The address of the recipient
-    /// @param _value The amount of tokens to be transfered
+    /// @param _amount The amount of tokens to be transfered
     /// @return Whether the transfer was successful or not
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _amount) returns (bool success);
 
-    /// @notice `msg.sender` approves `_spender` to spend `_value` tokens on its behalf
+    /// @notice `msg.sender` approves `_spender` to spend `_amount` tokens on its behalf
     /// @param _spender The address of the account able to transfer the tokens
-    /// @param _value The amount of tokens to be approved for transfer
+    /// @param _amount The amount of tokens to be approved for transfer
     /// @return Whether the approval was successful or not
-    function approve(address _spender, uint256 _value) returns (bool success);
+    function approve(address _spender, uint256 _amount) returns (bool success);
 
     /// @param _owner The address of the account owning tokens
     /// @param _spender The address of the account able to transfer the tokens
     /// @return Amount of remaining tokens of _owner that _spender is allowed to spend
     function allowance(address _owner, address _spender) constant returns (uint256 remaining);
 
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    event Transfer(address indexed _from, address indexed _to, uint256 _amount);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _amount);
 }
 
 contract Token is TokenInterface {
@@ -71,11 +71,11 @@ contract Token is TokenInterface {
     modifier noEther() {if (msg.value > 0) throw; _}
 
 
-    function transfer(address _to, uint256 _value) noEther returns (bool success) {
-        if (balances[msg.sender] >= _value && _value > 0) {
-            balances[msg.sender] -= _value;
-            balances[_to] += _value;
-            Transfer(msg.sender, _to, _value);
+    function transfer(address _to, uint256 _amount) noEther returns (bool success) {
+        if (balances[msg.sender] >= _amount && _amount > 0) {
+            balances[msg.sender] -= _amount;
+            balances[_to] += _amount;
+            Transfer(msg.sender, _to, _amount);
             return true;
         }
         else
@@ -83,12 +83,12 @@ contract Token is TokenInterface {
     }
 
 
-    function transferFrom(address _from, address _to, uint256 _value) noEther returns (bool success) {
-        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
-            balances[_to] += _value;
-            balances[_from] -= _value;
-            allowed[_from][msg.sender] -= _value;
-            Transfer(_from, _to, _value);
+    function transferFrom(address _from, address _to, uint256 _amount) noEther returns (bool success) {
+        if (balances[_from] >= _amount && allowed[_from][msg.sender] >= _amount && _amount > 0) {
+            balances[_to] += _amount;
+            balances[_from] -= _amount;
+            allowed[_from][msg.sender] -= _amount;
+            Transfer(_from, _to, _amount);
             return true;
         }
         else
@@ -96,9 +96,9 @@ contract Token is TokenInterface {
     }
 
 
-    function approve(address _spender, uint256 _value) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
+    function approve(address _spender, uint256 _amount) returns (bool success) {
+        allowed[msg.sender][_spender] = _amount;
+        Approval(msg.sender, _spender, _amount);
         return true;
     }
 

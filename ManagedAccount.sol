a7f64cbb5a2e1edb4bc7259636b0aa0f3248160c
Yoichi Hirai <i@yoichihirai.com>
2016-03-05 23:44:22 +0100

    Index `_recipient` in `PayOut` events.

--- ManagedAccount.sol
+++ ManagedAccount.sol
@@ -25,7 +25,7 @@ contract ManagedAccountInterface {
 
     function payOut(address _recipient, uint _amount) returns (bool);
 
-    event PayOut(address _recipient, uint _amount);
+    event PayOut(address indexed _recipient, uint _amount);
 }
 
 
